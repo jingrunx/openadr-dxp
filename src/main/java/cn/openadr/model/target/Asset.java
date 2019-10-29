3b31f27efe7768be7472653ab7ca966852c6ad6d
鲜景润 <jingrunx@hotmail.com>
2019-10-29 09:53:24 +0800

    +Asset.java

---
+++ src/main/java/cn/openadr/model/target/Asset.java
@@ -0,0 +1,13 @@
+package cn.openadr.model.target;
+
+public class Asset {
+	public String mrid;
+
+	public String getMrid() {
+		return mrid;
+	}
+
+	public void setMrid(String mrid) {
+		this.mrid = mrid;
+	}
+}
