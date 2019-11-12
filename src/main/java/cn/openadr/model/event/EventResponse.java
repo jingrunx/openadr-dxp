facb5c9d3b8878b6d9ab6d2fac68aed76ee4c8d2
鲜景润 <jingrunx@hotmail.com>
2019-11-12 09:11:37 +0800

    OptType @Json*Serializer

--- src/main/java/cn/openadr/model/event/EventResponse.java
+++ src/main/java/cn/openadr/model/event/EventResponse.java
@@ -2,7 +2,12 @@ package cn.openadr.model.event;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import cn.openadr.domain.OptType;
+import cn.openadr.domain.OptType.OptTypeDeserializer;
+import cn.openadr.jackson.EnumeratedSerializer;
 import cn.openadr.model.Response;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +17,9 @@ import lombok.NoArgsConstructor;
 public class EventResponse extends Response implements Serializable {
 	private static final long serialVersionUID = cn.openadr.Version.V1;
 
+	@JsonSerialize(using = EnumeratedSerializer.class)
+	@JsonDeserialize(using = OptTypeDeserializer.class)
 	public OptType optType;
+
 	public final QualifiedEventID qualifiedEventID = new QualifiedEventID();
 }
