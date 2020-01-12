3f7d6809267166d59d949f24245201cdaa27e792
鲜景润 <jingrunx@hotmail.com>
2020-01-13 07:21:07 +0800

    界面小调整

--- src/test/java/cn/openadr/JsonTest.java
+++ src/test/java/cn/openadr/JsonTest.java
@@ -1,19 +1,28 @@
 package cn.openadr;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.joda.JodaMapper;
+import cn.openadr.payload.DRObject;
+import cn.openadr.payload.reg.QueryRegistrationRequest;
 
 /**
  * Author: jrxian
  * Date: 2019-10-16 20:20
  */
 public class JsonTest {
-	public static void main(String[] args) throws JsonProcessingException {
+	public static void main(String[] args) throws IOException {
 		JodaMapper objectMapper = new JodaMapper();
 		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 		objectMapper.setWriteDatesAsTimestamps(false);
 
-//		System.out.println(objectMapper.writeValueAsString(event));
+		QueryRegistrationRequest request = new QueryRegistrationRequest();
+		String json = objectMapper.writeValueAsString(request);
+		System.out.println(json);
+
+		Object object = objectMapper.readValue(json, DRObject.class);
+		Class<?> objectClass = object.getClass();
+		System.out.println(objectClass.getName());
 	}
 }
